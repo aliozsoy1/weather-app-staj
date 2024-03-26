14242fba11b8beed8652db7cd851c56a72a6020d
aliozsoy1 <aliozsoy237@gmail.com>
2024-03-26 21:13:52 +0300

    search page design completed with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <div className="my-auto text-center px-3"> 
           <h1 className='heading-md mb-2'>Welcome to <span className='text-product'>TypeWeather</span></h1>
           <p className='text-base-300'>Choose a location to see the weather forecast</p>
-          <input className='bg-textbox-bg px-5 py-3 w-full mt-7 rounded-lg placeholder:text-base-400 text-white' name="myInput" placeholder='Search location'/>
+          <input className='bg-textbox-bg px-6 py-4 w-full mt-7 rounded-lg placeholder:text-base-400 text-white' name="myInput" placeholder='Search location'/>
         </div>
       </div>
     </>
