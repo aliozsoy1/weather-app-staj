48c7a0c6f3c55d7f1502411942c999bb6461db04
aliozsoy1 <aliozsoy237@gmail.com>
2024-03-26 21:12:45 +0300

    search page design completed with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
             <img src={viteLogo} className="logo" alt="Vite logo" />
           </a>
         </div>
-        <div className="my-auto text-center"> 
+        <div className="my-auto text-center px-3"> 
           <h1 className='heading-md mb-2'>Welcome to <span className='text-product'>TypeWeather</span></h1>
           <p className='text-base-300'>Choose a location to see the weather forecast</p>
           <input className='bg-textbox-bg px-5 py-3 w-full mt-7 rounded-lg placeholder:text-base-400 text-white' name="myInput" placeholder='Search location'/>
