a6effc2f3c8809edfeb9d83493a6ea456053a04a
aliozsoy1 <aliozsoy237@gmail.com>
2024-04-01 01:03:23 +0300

    creating details page

--- src/pages/details.jsx
+++ src/pages/details.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import '../App.css';
 
 function WeatherCityDetails() {
   const [cityDetails, setCityDetails] = useState(null);
@@ -18,17 +19,70 @@ function WeatherCityDetails() {
     };
 
     fetchCityDetails();
-  }, [cityName]);
+  }, [cityName, API_KEY]);
 
   if (!cityDetails) {
     return <div>Loading...</div>;
   }
 
+  const temperature = Math.floor(cityDetails.main.temp);
+  const weatherCondition = cityDetails.weather[0].main.toLowerCase();
+  const dayTime = isDayTime(cityDetails);
+
+  function isDayTime(details) {
+    const sunrise = details.sys.sunrise * 1000; // Convert to milliseconds
+    const sunset = details.sys.sunset * 1000; // Convert to milliseconds
+    const currentTime = Date.now();
+    return currentTime > sunrise && currentTime < sunset;
+  }
+
+  function getBackgroundClass(condition, day) {
+    if (day) {
+      switch (condition) {
+        case 'clear':
+          return 'bg-clear-day';
+        case 'clouds':
+          return 'bg-cloudy-day';
+        case 'rain':
+          return 'bg-rainy-day';
+        case 'snow':
+          return 'bg-snowy-day';
+        default:
+          return 'bg-default-day';
+      }
+    } else {
+      switch (condition) {
+        case 'clear':
+          return 'bg-clear-night';
+        case 'clouds':
+          return 'bg-cloudy-night';
+        case 'rain':
+          return 'bg-rainy-night';
+        case 'snow':
+          return 'bg-snowy-night';
+        default:
+          return 'bg-default-night';
+      }
+    }
+  }
+
+  const backgroundClass = getBackgroundClass(weatherCondition, dayTime);
+
+  const unixTimestamp = cityDetails.dt;
+  const date = new Date(unixTimestamp * 1000);
+  const dayIndex = date.getDay();
+  const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  const dayName = daysOfWeek[dayIndex];
+  const formattedDate = date.toLocaleDateString();
+
   return (
-    <div>
-      <h2 className='text-white'>{cityDetails.name}, {cityDetails.sys.country}</h2>
-      <h2 className='text-white'>{cityDetails.main.temp}</h2>
-      {/* Diğer detayları buraya ekleyebilirsiniz */}
+    <div className={"p-3 rounded-lg bg-weather-details-bg flex flex-col items-center justify-between"}>
+      <div className={`weather-details ${backgroundClass} p-3 rounded-lg bg-cover bg-no-repeat text-white`}>
+        <h2 className='text-heading-sm font-text-bold'>{cityDetails.name}, {cityDetails.sys.country}</h2>
+        <h3 className='text-heading-xs font-text-normal mb-20'>{dayName}, {formattedDate}</h3>
+        <h2>{temperature}°C</h2>
+        <h2>{cityDetails.weather[0].main}</h2>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors:{
-        'product' : '#8FB2F5',
-        'textbox-bg' : '#1E1E29',
-        'city-list' : '#3B3B54',
-        'base' : {
-          '900' : '#13131A',
-          '800' : '#16161F',
-          '700' : '#1C1C27',
-          '600' : '#22222F',
-          '500' : '#3B3B54',
-          '400' : '#7F7F98',
-          '300' : '#ABABC4',
-          '200' : '#BFBFD4',
-          '100' : '#FAFAFA',
-          'white' : '#FFFFFF',
+      colors: {
+        product: "#8FB2F5",
+        "textbox-bg": "#1E1E29",
+        "city-list": "#3B3B54",
+        "weather-details-bg": "#16161F",
+        base: {
+          900: "#13131A",
+          800: "#16161F",
+          700: "#1C1C27",
+          600: "#22222F",
+          500: "#3B3B54",
+          400: "#7F7F98",
+          300: "#ABABC4",
+          200: "#BFBFD4",
+          100: "#FAFAFA",
+          white: "#FFFFFF",
         },
-      }
+      },
+      backgroundImage: (theme) => ({
+        "clear-day": "url('./src/images/weatherbg-images/bg-clear-day.jpg')",
+        "clear-night": "url('./src/images/weatherbg-images/bg-clear-night.png')",
+        "cloudy-day": "url('./src/images/weatherbg-images/bg-cloudy-day.jpg')",
+        "cloudy-night": "url('./src/images/weatherbg-images/bg-cloudy-night.png')",
+      }),
+      fontSize: {
+        'heading-hg': '6rem', // 96px
+        'heading-xl': '3rem', // 48px
+        'heading-lg': '2rem', // 32px
+        'heading-md': '1.25rem', // 20px
+        'heading-sm': '1rem', // 16px
+        'heading-xs': '0.875rem', // 14px
+        'text-lg': '1.25rem', // 20px
+        'text-md': '1rem',    // 16px
+        'text-sm': '0.875rem', // 14px
+        'text-xs': '0.75rem',  // 12px
+      },
+      fontWeight: {
+        'text-normal': 400,  // Regular
+        'text-bold': 700,    // Bold
+        'heading-extrabold': 800,
+        'heading-bold': 700,
+      },
     },
   },
   plugins: [],
-}
-
+};
